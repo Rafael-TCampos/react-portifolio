dd13a74124b021701a62678c37b241d0a625abc6
Rafael-TCampos <rafael-cotonete2011@hotmail.com>
2024-09-24 13:20:41 -0300

    Finalizado a primeira secao

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,21 +1,25 @@
 import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+
 
 export const Hero = () => {
   return (
-    <section>
-      <div>
+    <section className={styles.container}>
+      <div className={styles.content}>
         <span>Olá, sou Willian.</span>
-        <h1>Product Designer</h1>
-        <p>
+        <h1 className={styles.title}>Product Designer</h1>
+        <p className={styles.description}>
           Como Especialista Júnior em Product Design, crio experiências
           significativas para os usuários, focando em interfaces intuitivas e
           atraentes.
         </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>Contato</a>
       </div>
-      <div>
-        <button>Visualizar Curriculo</button>
-        <button></button>
-      </div>
+      <img src={getImageUrl("hero/heroImage.png")} alt="minha foto" className={styles.heroImg}/>
+      <div className={styles.topBlur}/>
+      <div className={styles.bottomBlur}/>
     </section>
   );
 };
