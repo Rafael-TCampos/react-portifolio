321e53118c96d23e47202c36420a9164adff6180
Rafael-TCampos <rafael-cotonete2011@hotmail.com>
2024-09-29 22:05:43 -0300

    correcao do projeto

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">Willian Meireles</a>
+      <a className={styles.title} href="/">
+        Willian Meireles
+      </a>
       <div className={styles.menu}>
-        {/*Funcao para abrir o menu Amburguer*/} 
-        <img className={styles.menuBtn} src={menuOpen ? getImageUrl("nav/closeIcon.png") 
-          : getImageUrl("nav/menuIcon.png")
-        } alt="menu-button" onClick={() => setMenuOpen(!menuOpen)}/>
-        <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
+        {/*Funcao para abrir o menu Amburguer*/}
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
           <li>
             <a href="#home">Home</a>
           </li>
